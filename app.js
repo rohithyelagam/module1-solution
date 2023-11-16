562b2fbae521985744ed64965505cdbf09e9a27d
Rohith Yelagam <rohithyalagam2001@gmail.com>
2023-11-16 16:12:53 +0530

    final changes

---
+++ app.js
@@ -0,0 +1,36 @@
+(function(){
+    
+angular.module("LunchCheck",[])
+.controller("LunchCheckController",myController);
+
+myController.injection = ['%scope'];
+
+function myController($scope){
+
+    $scope.checkItems = function(){
+        if(!$scope.fooditems){
+            $scope.message = "Please enter data first";
+            return;
+        }
+        var foods = [];
+        var n = $scope.fooditems.length;
+        var item="";
+        for(var i=0;i<n;i++){
+            if($scope.fooditems[i]==','){
+                if(item!="")foods.push(item);
+                item="";
+            }else{
+                item+=$scope.fooditems[i];
+            }
+        }
+        if(item!="")foods.push(item);
+        if(foods.length == 0){
+            $scope.message = "Please enter data first";
+        }else if(foods.length<=3){
+            $scope.message = "Enjoy!";
+        }else{
+            $scope.message = "Too much!";
+        }
+    }
+}
+})();
